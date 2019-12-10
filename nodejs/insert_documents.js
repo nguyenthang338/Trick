215526b6f67815b38388a4aafb241c70bdd0a5d7
nguyenthang338 <nguyenthang33894@gmail.com>
2019-12-10 14:11:59 +0700

    add insert_documents.js

---
+++ nodejs/insert_documents.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb://localhost:27017';
+const dnName = 'nvthang';
+
+( async function(){
+  let client;
+
+  try{
+    client = await MongoClient.connect(url);
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+    //Insert a single document
+    //
+    let r = await db.collection('inserts').insertOne({a:1});
+
+    //insert multiple doucments
+    r = await db.collection('inserts').insertMany([{a:2}, {a:3}, {a:4}]);
+  } catch(err){
+    console.log(err.stack);
+  }
+
+  client.close();
+
+})();
+
